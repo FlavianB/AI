dd9b2a0499106dca0dbc2e0e58631646c72dade0
Paul <cirstean.paul.ioan@gmail.com>
2024-11-04 20:22:33 +0200

    Added validation

--- models/classroom.py
+++ models/classroom.py
@@ -1,5 +1,7 @@
+from curses.ascii import isspace
 from enum import Enum
 from returns.result import Result, Success, Failure
+from returns.pipeline import is_successful
 
 import models.constants as consts
 
@@ -13,7 +15,7 @@ class ClassroomType(Enum):
         try:
             return Success(ClassroomType(value))
         except ValueError:
-            return Failure(f"Invalid classroom type {value}")
+            return Failure(f"Invalid classroom type {value}.\n")
 
 class Classroom:
     """
@@ -26,6 +28,7 @@ class Classroom:
         
         Use the `create` method to instantiate a Classroom object.
     """
+
     __slots__ = '__id', '__type', 'availability'
     __id: str
     __type: ClassroomType
@@ -35,27 +38,38 @@ class Classroom:
     def create(id_: str, type_: str) -> Result["Classroom", str]:
         """
         Creates a Classroom object. In case of 
-        an invalid type, it will return an object with the field `type`
-        equal to `ClassroomType.INVALID`.
+        an invalid type, or empty id it will return a error message.
 
         Parameters:
             id_ (str): Id of the classroom.
-            type_ (str): Type of the classroom.
+            type (str): Type of the classroom.
             
         Returns:
-            Classroom: The object representing the classroom.
+            Result: The object representing the classroom or a error message.
         """
-        clas = Classroom(id_)
-        clas.__id = id_
-        resultType =  ClassroomType.from_string(type_)
-        match resultType:
-            case Success(type):
-                clas.__type = type
-            case Failure(err):
-                return Failure(err) 
+
+        err = ''
+        if isspace(id_):
+            err += 'Id of the class cannot be empty.\n'
         
-        clas.availability = consts.BASIC_AVAILABILITY.copy()
-        return Success(clas)
+        resultType =  ClassroomType.from_string(type_)
+
+        if not is_successful(resultType):
+            err += resultType.failure()
+
+        if err:
+            return Failure(err)
+        
+        return Success(Classroom(id_, resultType.unwrap()))
+    
+    def __init__(self, id_: str, type: ClassroomType) -> None:
+        """
+        DO NOT USE THIS METHOD OUTSIDE the `Classroom` class.
+        """
+
+        self.__id = id_
+        self.__type = type
+        self.availability = consts.BASIC_AVAILABILITY
 
     def get_id(self) -> str:
         return self.__id
@@ -65,15 +79,9 @@ class Classroom:
 
     def __str__(self):
         availability_str = "\n".join([f"{day}: {', '.join(intervals)}" for day, intervals in self.availability.items()])
+
         return (
             f"Classroom: {self.__id}\n"
             f"Type: {self.__type.name}\n"
             f"Availability:\n{availability_str}"
         )
-    
-    def __init__(self, id_: str) -> None:
-        """
-        DO NOT USE THIS METHOD OUTSIDE the `Classroom` class.
-        """
-        self.__id = id_
-        self.availability = consts.BASIC_AVAILABILITY
--- models/course.py
+++ models/course.py
@@ -2,6 +2,8 @@
 
 from enum import Enum
 from typing import Optional
+
+from models.event import Event
 # from models.classroom import Classroom
 # from models.staff_member import StaffMember
 
@@ -11,9 +13,12 @@ class CourseType(Enum):
     Laboratory = 'Laboratory'
 
 class Course:
-    event_id: str
-    type: CourseType
-    group: str
-    optional_package: Optional[int]
-    instructors: list[int] 
+    __event_id: str
+    __type: CourseType
+    __group: str
+    __optional_package: Optional[int]
+    __instructors: list[int] 
+
+    def __init__(self, event: Event):
+        self.__event_id = event.get_id()
     # Taking type in consideration instructors are either primary or secondary 
